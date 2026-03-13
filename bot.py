150060d0c5c6f4827aae86d0e96044f0660d9fdc
fernandescaua-ai <fernandes.caua@gmail.com>
2026-03-13 18:59:08 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,9 @@
 import requests
-import time
 import os
+
+print("Bot iniciou")
 
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
 CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
 
-URL = "https://immi.homeaffairs.gov.au/what-we-do/whm-program/status-of-country-caps"
-
-def send_message(msg):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    data = {"chat_id": CHAT_ID, "text": msg}
-    requests.post(url, data=data)
-
-print("Bot iniciado...")
-
-r = requests.get(URL)
-text = r.text.lower()
-
-if "brazil" in text and "open" in text:
-    send_message("🚨 VISTO 462 ABRIU PARA O BRASIL!")
-else:
-    print("Ainda fechado...")
+print("Token carregado")
